926c79c8e464a0bcc32b6665d30829b198a66dbd
Martin William <martinsoftwaredev@outlook.com>
2023-07-15 18:20:42 +0100

    remove cookiebot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import SupabaseAuthProvider from '@/utils/providers/supabase-auth-provider';
 import { createClient } from '@/lib/supabase/supabase-server';
 import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
-import Head from 'next/head';
 import GAnalytics from '@/lib/google/analytics';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -29,15 +28,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className="text-[--dark-blue-3] bg-[--light-blue-1]">
-      <Head>
-        <script
-          id="Cookiebot"
-          src="https://consent.cookiebot.com/uc.js"
-          data-cbid="282d6d86-0b13-48b9-8d4c-48b99e1c5e21"
-          data-blockingmode="auto"
-          type="text/javascript"
-        ></script>
-      </Head>
       <body className={inter.className}>
         <SupabaseProvider>
           <SupabaseAuthProvider serverSession={session}>
