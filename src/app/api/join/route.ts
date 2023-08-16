da0afc4b261bf1a14f45679c6466f103cf7e5259
Martin William <martinsoftwaredev@outlook.com>
2023-08-16 16:25:12 +0100

    re work join api to reflect new type and rpc changes

--- src/app/api/join/route.ts
+++ src/app/api/join/route.ts
@@ -1,29 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { get_list_from_short_id } from '@/utils/types/collections.types';
 
 export async function PUT(req: NextRequest) {
   const { shortId } = await req.json();
-  console.log(shortId, "shortId")
   const supabase = createRouteHandlerClient({ cookies });
   const { data: user } = await supabase.auth.getUser();
-  const response = await supabase.rpc('get_list_from_short_id', {
+  const result = await supabase.rpc('get_list_from_short_id', {
     shortid: shortId,
   });
-  console.log(response, "response")
 
-  // tidy up logic
-  if (response.error) {
-    return NextResponse.json({ error: response.error });
-  } else if (!response.error) {
-    const { data, error } = await supabase
+  if (result.error) {
+    console.log(result.error);
+    return;
+  }
+  const list = result.data as get_list_from_short_id["Returns"];
+  const { id } = list[0];
+  const { data, error } = await supabase
       .from('list_participants')
-      .insert({ list_id: response.data.id, participant_id: user?.user?.id });
-      console.log(error,  "error")
+      .insert({ list_id: id, participant_id: user?.user?.id });
+
     if (error) {
       return NextResponse.json({ error });
     } else {
       return NextResponse.json({ data });
     }
-  }
 }
