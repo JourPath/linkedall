33325bd2bc50c41b6de99e9809806189a420558f
Martin William <martinsoftwaredev@outlook.com>
2023-08-16 16:19:56 +0100

    console log all in join api

--- src/app/api/join/route.ts
+++ src/app/api/join/route.ts
@@ -4,12 +4,13 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 
 export async function PUT(req: NextRequest) {
   const { shortId } = await req.json();
+  console.log(shortId, "shortId")
   const supabase = createRouteHandlerClient({ cookies });
   const { data: user } = await supabase.auth.getUser();
   const response = await supabase.rpc('get_list_from_short_id', {
     shortid: shortId,
   });
-  console.log(response)
+  console.log(response, "response")
 
   // tidy up logic
   if (response.error) {
@@ -18,7 +19,7 @@ export async function PUT(req: NextRequest) {
     const { data, error } = await supabase
       .from('list_participants')
       .insert({ list_id: response.data.id, participant_id: user?.user?.id });
-      console.log(error)
+      console.log(error,  "error")
     if (error) {
       return NextResponse.json({ error });
     } else {
