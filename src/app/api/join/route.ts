ca6a8be34d603c4061101152f3f139a77e14887b
Martin William <martinsoftwaredev@outlook.com>
2023-08-16 17:17:10 +0100

    add console log for all

--- src/app/api/join/route.ts
+++ src/app/api/join/route.ts
@@ -5,8 +5,11 @@ import { get_list_from_short_id } from '@/utils/types/collections.types';
 
 export async function PUT(req: NextRequest) {
   const { shortId } = await req.json();
+  console.log(shortId, 'shortId')
   const supabase = createRouteHandlerClient({ cookies });
+  console.log(supabase, 'supabase')
   const { data: user } = await supabase.auth.getUser();
+  console.log(user, 'user')
   const result = await supabase.rpc('get_list_from_short_id', {
     shortid: shortId,
   });
@@ -19,6 +22,7 @@ export async function PUT(req: NextRequest) {
   const list = result.data as get_list_from_short_id["Returns"];
   console.log(list,  'list');
   const { id } = list[0];
+  console.log(id, 'id')
   const { data, error } = await supabase
       .from('list_participants')
       .insert({ list_id: id, participant_id: user?.user?.id });
