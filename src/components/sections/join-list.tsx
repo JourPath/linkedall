60e559d3c3099e5e11fdfd2540d917ad777d0444
Martin William <martinsoftwaredev@outlook.com>
2023-08-17 14:27:00 +0100

    re work joinlist to remove state refreshes

--- src/components/sections/join-list.tsx
+++ src/components/sections/join-list.tsx
@@ -7,34 +7,46 @@ import { useEffect, useState } from "react";
 export default function JoinList() {
   const [shortId, setShortId] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [joinListError, setJoinListError] = useState<string | null>(null);
   const { user } = useAuth();
 
   useEffect(() => {
-    setIsLoading(false);
-  }, [user?.linked_in]);
+    setIsLoading(!user);
+  }, [user]);
 
-  async function joinList() {
-    const response = await fetch("https://www.linkedall.online/api/join", {
-      method: "PUT",
-      body: JSON.stringify({ shortId }),
-    });
-    const data = await response.json();
-    return data;
-  }
+  const joinList = async () => {
+    try {
+      const response = await fetch("https://www.linkedall.online/api/join", {
+        method: "PUT",
+        body: JSON.stringify({ shortId }),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || "An error occurred.");
+      }
+      return data;
+    } catch (err) {
+      if (err instanceof Error) {
+        setJoinListError(err.message);
+      } else {
+        setJoinListError("An unexpected error occurred.");
+      }
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+      if (isLoading) {
+        return <p>Loading...</p>;
+      }
+    }
 
-  if (!user?.linked_in) {
-    return (
-      <Link href="/profile">
-        <p className="bg-amber-300 py-4 text-center w-full">
-          Set LinkedIn URL to join lists
-        </p>
-      </Link>
-    );
-  } else {
+    if (!user?.linked_in) {
+      return (
+        <Link href="/profile">
+          <p className="bg-amber-300 py-4 text-center w-full">
+            Set LinkedIn URL to join lists
+          </p>
+        </Link>
+      );
+    }
+
     return (
       <div className="bg-[--white] h-12 flex flex-row items-center justify-between rounded-full mx-2 mb-2 my-4 lg:mx-0">
         <input
@@ -47,7 +59,8 @@ export default function JoinList() {
           onChange={(e) => {
             return setShortId(e.target.value.toUpperCase());
           }}
-        ></input>
+        />
+        {joinListError && <p>Error: {joinListError}</p>}
         <button
           onClick={joinList}
           className="bg-[--blue-2] rounded-r-full text-[--white] h-12 w-1/4"
@@ -56,5 +69,5 @@ export default function JoinList() {
         </button>
       </div>
     );
-  }
+  };
 }
