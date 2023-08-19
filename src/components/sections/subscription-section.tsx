10494e0bff37be4f46c2ee39b2448b478c3f8532
Martin William <martinsoftwaredev@outlook.com>
2023-08-19 19:31:09 +0100

    check user before getting customer data in profile

--- src/components/sections/subscription-section.tsx
+++ src/components/sections/subscription-section.tsx
@@ -39,7 +39,9 @@ export default async function SubscriptionSection() {
         setCustomer(data);
       }
     };
-    customerData();
+    if (user) {
+      customerData();
+    }
   }, [user]);
 
   if (isLoading) {
