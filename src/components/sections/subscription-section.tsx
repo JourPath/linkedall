ee31919e5c8b7b820956b9e84a4ea41403e7596d
Martin William <martinsoftwaredev@outlook.com>
2023-08-19 20:41:55 +0100

    add user to dependency

--- src/components/sections/subscription-section.tsx
+++ src/components/sections/subscription-section.tsx
@@ -57,7 +57,7 @@ export default async function SubscriptionSection() {
     };
 
     fetchCustomerData();
-  }, []);
+  }, [user]);
 
   useEffect(() => {
     const redirectToStripe = async () => {
