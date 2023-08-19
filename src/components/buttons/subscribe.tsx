e2a91326ee78fc84d2a1074cb040bcb322d93c45
Martin William <martinsoftwaredev@outlook.com>
2023-08-19 13:43:28 +0100

    update subscription text

--- src/components/buttons/subscribe.tsx
+++ src/components/buttons/subscribe.tsx
@@ -4,22 +4,39 @@ import { createClient } from '@/lib/supabase/supabase-browser';
 import { useAuth } from '@/utils/providers/supabase-auth-provider';
 import { loadStripe } from '@stripe/stripe-js';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Subscribe({ planId }: { planId: string }) {
   const { user } = useAuth();
+  const [planText, setPlanText] = useState<string>('Subscribe');
+  const [plan, setPlan] = useState<string | null>(null);
   const router = useRouter();
   const supabase = createClient();
 
-  async function handleSubscribe(id: string) {
-    if (!user) {
-      router.push(`/signup?plan=${id}`);
-    } else {
+  useEffect(() => {
+    const customerData = async function () {
       const planRes = await supabase
         .from('customers')
         .select('plan')
         .eq('id', user?.id)
         .single();
       const plan = planRes.data?.plan;
+      setPlan(plan!);
+      if (plan === 'BASIC') {
+        setPlanText('Upgrade Subscription');
+      } else {
+        setPlanText('Manage Subscription');
+      }
+    };
+    if (user) {
+      customerData();
+    }
+  }, [user]);
+
+  async function handleSubscribe(id: string) {
+    if (!user) {
+      router.push(`/signup?plan=${id}`);
+    } else {
       if (plan !== 'BASIC') {
         const response = await fetch(
           'https://www.linkedall.online/api/stripe/portal',
@@ -45,5 +62,5 @@ export default function Subscribe({ planId }: { planId: string }) {
     }
   }
 
-  return <button onClick={() => handleSubscribe(planId)}>Subscribe</button>;
+  return <button onClick={() => handleSubscribe(planId)}>{planText}</button>;
 }
