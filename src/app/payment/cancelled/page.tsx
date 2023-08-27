867c8444196c62a810149ca4a586201567bbd1c5
Martin William <martinsoftwaredev@outlook.com>
2023-08-27 22:15:04 +0100

    add forwarding to failure payment

--- src/app/payment/cancelled/page.tsx
+++ src/app/payment/cancelled/page.tsx
@@ -1,12 +1,13 @@
-export default function Cancelled(){
-    return (
-        <div className="w-full max-w-3xl mx-auto py-16 flex justify-around">
-        <div className="w-80 h-40 rounded shadow px-6 py-8">
-            <h2>Payment Cancelled</h2>
-            <p>
-            You have not been charged
-            </p>
-        </div>
-        </div>
-    )
+import Forward from '@/components/buttons/forward';
+
+export default function Cancelled() {
+  return (
+    <div className="w-full max-w-3xl mx-auto py-16 flex justify-around">
+      <div className="rounded shadow px-6 py-8">
+        <h2>Payment Cancelled</h2>
+        <p>You have not been charged</p>
+        <Forward time={3000} path={'pricing'} />
+      </div>
+    </div>
+  );
 }
--- src/components/buttons/forward.tsx
+++ src/components/buttons/forward.tsx
@@ -3,24 +3,28 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
-export default function ProfileForward() {
+export default function Forward({
+  time,
+  path,
+}: {
+  time: number;
+  path: string;
+}) {
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('/profile');
-    }, 3000);
+      router.push(`/${path}`);
+    }, time);
     return () => clearTimeout(timer);
   });
 
   return (
     <>
-      <p>
-        You will be shortly transferred to your profile, otherwise click below.
-      </p>
+      <p>You will be shortly transferred to {path} otherwise click below.</p>
       <Link href={`/profile`} className="w-full h-12 justify-center flex">
         <button className="bg-[--blue-2] rounded-full text-[--white] h-12 w-1/2 my-4">
-          Go to Profile
+          Go to {path}
         </button>
       </Link>
     </>
