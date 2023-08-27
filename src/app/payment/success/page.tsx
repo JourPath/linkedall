e0fa018d229bd3e6520015ac6e60617afcf8809b
Martin William <martinsoftwaredev@outlook.com>
2023-08-27 21:47:32 +0100

    add forwarding to success page

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,12 +1,13 @@
+import ProfileForward from '@/components/buttons/profile-forward';
+
 export default function Success() {
-    return (
-        <div className="w-full max-w-3xl mx-auto py-16 flex justify-around">
-        <div className="w-80 h-40 rounded shadow px-6 py-8">
-            <h2>Success</h2>
-            <p>
-            Thank you for subscribing!
-            </p>
-        </div>
-        </div>
-    );
+  return (
+    <div className="w-full max-w-3xl mx-auto py-16 flex justify-around">
+      <div className="rounded shadow px-6 py-8">
+        <h2>Success</h2>
+        <p>Thank you for subscribing!</p>
+        <ProfileForward />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/buttons/profile-forward.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+export default function ProfileForward() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/profile');
+    }, 3000);
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <>
+      <p>
+        You will be shortly transferred to your profile, otherwise click below.
+      </p>
+      <Link href={`/profile`} className="w-full h-12 justify-center flex">
+        <button className="bg-[--blue-2] rounded-full text-[--white] h-12 w-1/2 my-4">
+          Go to Profile
+        </button>
+      </Link>
+    </>
+  );
+}
